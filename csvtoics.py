41188c657e77e9d6927e2a55ed6e9d0b352ad2fb
Attila Bukor <abukor@apache.org>
2019-02-01 15:09:00 +0100

    Update to 2018/19 format

--- csvtoics.py
+++ csvtoics.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2.7
+#!/usr/bin/env python
 
 import csv
 import sys
@@ -8,7 +8,6 @@ from datetime import datetime
 import pytz
 
 
-year = datetime.now().year
 tz = timezone('Europe/Budapest')
 cal = Calendar()
 
@@ -18,21 +17,23 @@ cal.add('version', '2.0')
 i = 0
 with open(sys.argv[1], 'rb') as csvfile:
     next(csvfile)
-    reader = csv.reader(csvfile)
+    reader = csv.reader(csvfile, delimiter=';')
     for row in reader:
         i += 1
-        date = row[0].rsplit('/')
-        times = row[1].rsplit('-')
+        if row[0] == '':
+            continue
+        date = row[0].rsplit('.')
+        times = row[2].rsplit('-')
         start = times[0].rsplit(':')
         end = times[1].rsplit(':')
         event = Event()
         event.add('summary', row[4])
-        event.add('uid', str(year) + date[0] + date[1] + 'T' + start[0] + start[1] + '/' + str(i) + '@r1pp3rj4ck.eu')
-        event.add('dtstart', datetime(year, int(date[0]), int(date[1]), int(start[0]), int(start[1]), 0, tzinfo=tz))
-        event.add('dtend', datetime(year, int(date[0]), int(date[1]), int(end[0]), int(end[1]), 0, tzinfo=tz))
+        event.add('uid', date[0] + date[1] + date[2] + 'T' + start[0] + start[1] + '/' + str(i) + '@bukor.me')
+        event.add('dtstart', datetime(int(date[0]), int(date[1]), int(date[2]), int(start[0]), int(start[1]), 0, tzinfo=tz))
+        event.add('dtend', datetime(int(date[0]), int(date[1]), int(date[2]), int(end[0]), int(end[1]), 0, tzinfo=tz))
         event.add('dtstamp', datetime.now())
-        event.add('location', vText('Budapesti Corvinus Egyetem, ' + row[7]))
-        event.add('description', vText(row[6]))
+        event.add('location', vText('Budapesti Corvinus Egyetem, ' + row[6]))
+        event.add('description', vText(row[5]))
         cal.add_component(event)
 
 f = open(sys.argv[2], 'wb')
